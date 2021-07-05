1f17ebdc20e456e80772e3719a325bc38ee30b91
Eliott <eliott_nu@mail.ru>
2021-07-05 20:38:04 +0900

    Displaying the change in the number of lives

--- To stand to the last/Assets/Scripts/EnemyDestination.cs
+++ To stand to the last/Assets/Scripts/EnemyDestination.cs
@@ -1,9 +1,19 @@
+using System;
 using UnityEngine;
 
 public class EnemyDestination : MonoBehaviour
 {
+    private UIDisplayStats _stats;
+
+    private void Awake()
+    {
+        if (Camera.main is { }) _stats = Camera.main.GetComponent<UIDisplayStats>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Enemy")) Destroy(other.gameObject);
+        if (!other.CompareTag("Enemy")) return;
+        _stats.GetDamage();
+        Destroy(other.gameObject);
     }
 }
---
+++ To stand to the last/Assets/Scripts/UIDisplayStats.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDisplayStats : MonoBehaviour
+{
+    [Header("Set in Inspector:")] 
+    [SerializeField] private Text healthCounter;
+    [SerializeField] private Text goldCounter;
+
+    [Header("Set Options:")] 
+    public int health = 20;
+    
+    private void Update()
+    {
+        healthCounter.text = health.ToString();
+    }
+    
+    public void GetDamage(int damage = 1)
+    {
+        health -= damage;
+    }
+}
