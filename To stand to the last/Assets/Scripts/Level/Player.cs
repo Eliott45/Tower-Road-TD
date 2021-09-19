f46044a6f94b589b49ae82e3c7ed9e3906169ec6
Maksim <eliott_nu@mail.ru>
2021-09-19 17:32:45 +0900

    LifeCounter UI

--- To stand to the last/Assets/Scripts/Level/Player.cs
+++ To stand to the last/Assets/Scripts/Level/Player.cs
@@ -1,16 +1,27 @@
+using System;
 using UnityEngine;
 
 namespace Level
 {
     public class Player : MonoBehaviour
     {
+        [Header("Set in Inspector:")]
+        [SerializeField] private UIPlayerStats _UIPlayerStats;
+        
         [Header("Set player options: ")] 
-        [SerializeField] private float _health;
+        [SerializeField] private int _health;
         [SerializeField] private int _gold;
+
+        private void Awake()
+        {
+            _UIPlayerStats.UpdateHealthCounter(_health);
+            _UIPlayerStats.UpdateGoldCounter(_gold);
+        }
 
         public void GetDamage(int damage)
         {
             _health -= damage;
+            _UIPlayerStats.UpdateHealthCounter(_health);
         }
 
         private void GetGold(int gold)
---
+++ To stand to the last/Assets/Scripts/Level/UIPlayerStats.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Level
+{
+    public class UIPlayerStats : MonoBehaviour
+    {
+        [Header("Set in Inspector:")] 
+        [SerializeField] private TMP_Text _healthText;
+        [SerializeField] private TMP_Text _goldText;
+
+        public void UpdateHealthCounter(int health)
+        {
+            _healthText.text = health.ToString();
+        }
+        
+        public void UpdateGoldCounter(int gold)
+        {
+            _goldText.text = gold.ToString();
+        }
+        
+    }
+}
